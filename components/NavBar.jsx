24d1690debadcd967f7e0d286942d6846f518d53
DELL <kuntualaricarka@gmail.com>
2025-02-25 12:41:18 -0500

    Link a la place de button

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function NavBar({ changePage }) {
-    return (
-     <ul className="hidden md:flex justify-center gap-x-8 flex-grow">
-      <li>
-       <button onClick={() => changePage("home")}>Accueil</button>
-      </li>
-      <li>
-       <button onClick={() => changePage("programme")}>Programme</button>
-      </li>
-      
-     </ul>
-    );
-   }
+ return (
+  <ul className="hidden md:flex justify-center gap-x-8 flex-grow">
+   <li>
+    <Link to="/" onClick={() => changePage("home")}>
+     Accueil
+    </Link>
+   </li>
+   <li>
+    <Link to="/programme" onClick={() => changePage("programme")}>
+     Programme
+    </Link>
+   </li>
+  </ul>
+ );
+}
