825ae12e84c35e34879cd2a34f47f5241e17835d
glo-22 <gloria.nanamoukam@gmail.com>
2025-03-25 16:09:22 +0100

    new modif

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,15 @@
 import Contact from "@/components/Contact";
+
+export const metadata = {
+  metadataBase: new URL('http://acme.com'),
+  title: 'Contact | app Conference',
+  description: 'Page de Contact',
+  openGraph: {
+    title: 'Contact | app Conference',
+    description: 'Page de Contact',
+  },
+};
+
 export default function Page() {
  return <Contact />;
 }
